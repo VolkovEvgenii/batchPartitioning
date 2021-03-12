43400c5f52e8c86141994fc4b776ec9426251528
evgeniy volkov <volkov.e.n@gmail.com>
2021-03-12 10:25:19 +0300

    init commit

---
+++ src/test/java/ru/volkov/batch/partitioning/PartitioningApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.volkov.batch.partitioning;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PartitioningApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
